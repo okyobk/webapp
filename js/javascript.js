c1143e137725c426505c9783852aa54bed0eec4b
okyobk <okyobk@gmail.com>
2014-07-21 19:38:53 +0700

    add project

--- js/javascript.js
+++ js/javascript.js
@@ -1,36 +1,8 @@
 
 $( document ).ready(function(){
 	// localStorage.clear();
-	if (localStorage.getItem("input2")!= null) {
-	    var mySplitResult = localStorage.getItem("input2").split(":");
-    	for (var i = 0; i < mySplitResult.length ; i++) {
-    		if (mySplitResult.length<2) {
-    			$("#"+mySplitResult[i]).html(mySplitResult[i+1]);
-    			break;
-    		}
-    		else{
-    			$("#"+mySplitResult[i]).html(mySplitResult[i+1]);
-    		}
-		}
-	}
-
-	if (localStorage.getItem("lat")!= null) {
-	    var mylat = localStorage.getItem("lat").split(":");
-    	for (var t = 0; t < mylat.length ; t++) {
-    		if (mylat.length<2) {
-    			$("#"+mylat[t]).html(mylat[t+1]);
-    			break;
-    		}
-    		else{
-    			$("#"+mylat[t]).html(mylat[t+1]);
-    		}
-		}
-	}
-
-	if (localStorage.getItem("name1")!= null){
-		$('#name1').val(localStorage.getItem("name1"))
-	}
-
+	
+	//create data in page index1.html
 	var model1 = new Array;
 		model1[1] = "HOYALUX  MSV　マイルド　16mm";
 		model1[2] = "HOYALUX  MSV　マイルド　14mm";
@@ -102,29 +74,65 @@ $( document ).ready(function(){
 		model[5] = "PENTAX";
 		model[6] = "その他";
 
-
-
 	var k=0;
-		for(var i = 1; i <= 6; i++) {
-    		 $('.label3').html($('.label3').html()+ '<div id="cell' + i + '" class="cell">'+model[i]+'</div><br>' );
-    		 if(k==0){
-    		 	for(var t = 1; t <= 10; t++) {
-    		 	 $('.label4').html($('.label4').html()+ '<div id="cell' + t + '" class="row" name="'+model1[t]+'">'+model1[t]+'</div><br>' );
-    		 	}
-    		 	$('.row').click(function(){
-   						var name=$(this).attr("name");
-            		});
-    		 }
-   		}
+	var lat1;
+	var rel1;
+	var u=0;
+	var rel3;
+	var rel4;
+	var run;
+	var name1="row1";
+	var index1= window.location.pathname;
+
+	//load data from page index.html to page index3.html
+	if (localStorage.getItem("input2")!= null) {
+	    var mySplitResult = localStorage.getItem("input2").split(":");
+    	for (var i = 0; i < mySplitResult.length ; i++) {
+    		if (mySplitResult.length<2) {
+    			$("#"+mySplitResult[i]).html(mySplitResult[i+1]);
+    			break;
+    		}
+    		else{
+    			$("#"+mySplitResult[i]).html(mySplitResult[i+1]);
+    		}
+		}
+	}
+
+	//load data from page index2.html to page index3.html
+	if (localStorage.getItem("lat")!= null) {
+	    var mylat = localStorage.getItem("lat").split(":");
+    	for (var t = 0; t < mylat.length ; t++) {
+    		if (mylat.length<2) {
+    			$("#"+mylat[t]).html(mylat[t+1]);
+    			break;
+    		}
+    		else{
+    			$("#"+mylat[t]).html(mylat[t+1]);
+    		}
+		}
+	}
+
+	if (localStorage.getItem("name1")!= null){
+		$('#name1').val(localStorage.getItem("name1"))
+	}
+
+	if (index1.search('index1') > -1 ) {
+		console.log(index1);
+		loadlabel();
+	}
+		
    		if (!id) {
    			var id="cell1";
    		}
-   		
-   		if (document.getElementById(id)!=null)
-   		// document.getElementById(id).style.backgroundColor = "red";
+
+   		//show data page index1.html
   		$('.cell').click(function(){
+  			if (localStorage.getItem("cell")!=null) {
+  				document.getElementById(localStorage.getItem("cell")).style.backgroundColor ="white";
+  			}
   			k=1;
             var res=$(this).attr("id");
+            
             if (document.getElementById(res).style.backgroundColor = "white") {
             	if ( res!=id) {
             	document.getElementById(id).style.backgroundColor = "white"
@@ -148,32 +156,12 @@ $( document ).ready(function(){
     		 			$('.label4').html($('.label4').html()+ '<div id="row' + j + '" class="row" name="'+model1[j]+'">'+model1[j]+'</div><br>' );
    					}
 
-
-   					var name1="row1";
-   					// document.getElementById(name1).style.backgroundColor = "red";
    					$('.row').click(function(){
-   						var name=$(this).attr("name");
-   						var name2=$(this).attr("id");
-   						document.getElementById(name2).style.backgroundColor = "red";
-
-   						if (document.getElementById(name2).style.backgroundColor = "white") {
-	            		if ( name2!=name1) {
-			            	document.getElementById(name1).style.backgroundColor = "white"
-			            	document.getElementById(name2).style.backgroundColor = "red";
-			            	localStorage.removeItem(name1);
-            				localStorage.setItem("row",name2);
-	            			name1=name2;
-	           			}
-		            	else{
-		            		localStorage.setItem("row",name1);
-		            	 	document.getElementById(name2).style.backgroundColor = "red";
-		            	}
-		            }
-		            	localStorage.setItem("rownew",name);
-   						$('.label2').html(name);
+   						var pa1 = $(this).attr("name");
+   						var pa2 = $(this).attr("id");
+   						changecolor(pa1,pa2);
+   						
             		});
-
-
 
             	break;
 
@@ -183,28 +171,10 @@ $( document ).ready(function(){
             		for(var j = 1; j <= 11; j++) {
     		 			$('.label4').html($('.label4').html()+ '<div id="row' + j + '" class="row" name="'+model2[j]+'">'+model2[j]+'</div><br>' );
    					}
-   					var name1="row1";
-   					// document.getElementById(name1).style.backgroundColor = "red";
    					$('.row').click(function(){
-   						var name=$(this).attr("name");
-   						var name2=$(this).attr("id");
-   						document.getElementById(name2).style.backgroundColor = "red";
-
-   						if (document.getElementById(name2).style.backgroundColor = "white") {
-	            		if ( name2!=name1) {
-			            	document.getElementById(name1).style.backgroundColor = "white"
-			            	document.getElementById(name2).style.backgroundColor = "red";
-			            	localStorage.removeItem(name1);
-            				localStorage.setItem("row",name2);
-	            			name1=name2;
-	           			}
-		            	else{
-		            		localStorage.setItem("row",name1);
-		            	 	document.getElementById(name2).style.backgroundColor = "red";
-		            	}
-		            }
-		            	localStorage.setItem("rownew",name);
-   						$('.label2').html(name);
+   						var pa1 = $(this).attr("name");
+   						var pa2 = $(this).attr("id");
+   						changecolor(pa1,pa2);
             		});
 
             	break;
@@ -215,28 +185,10 @@ $( document ).ready(function(){
             		for(var j = 1; j <= 11; j++) {
     		 			$('.label4').html($('.label4').html()+ '<div id="row' + j + '" class="row" name="'+model3[j]+'">'+model3[j]+'</div><br>' );
    					}
-   					var name1="row1";
-   					// document.getElementById(name1).style.backgroundColor = "red";
    					$('.row').click(function(){
-   						var name=$(this).attr("name");
-   						var name2=$(this).attr("id");
-   						document.getElementById(name2).style.backgroundColor = "red";
-
-   						if (document.getElementById(name2).style.backgroundColor = "white") {
-	            		if ( name2!=name1) {
-			            	document.getElementById(name1).style.backgroundColor = "white"
-			            	document.getElementById(name2).style.backgroundColor = "red";
-			            	localStorage.removeItem(name1);
-            				localStorage.setItem("row",name2);
-	            			name1=name2;
-	           			}
-		            	else{
-		            		localStorage.setItem("row",name1);
-		            	 	document.getElementById(name2).style.backgroundColor = "red";
-		            	}
-		            }
-		            	localStorage.setItem("rownew",name);
-   						$('.label2').html(name);
+   						var pa1 = $(this).attr("name");
+   						var pa2 = $(this).attr("id");
+   						changecolor(pa1,pa2);
             		});
 
             	break;
@@ -247,28 +199,10 @@ $( document ).ready(function(){
             		for(var j = 1; j <= 10; j++) {
     		 			$('.label4').html($('.label4').html()+ '<div id="row' + j + '" class="row" name="'+model4[j]+'">'+model4[j]+'</div><br>' );
    					}
-   					var name1="row1";
-   					// document.getElementById(name1).style.backgroundColor = "red";
    					$('.row').click(function(){
-   						var name=$(this).attr("name");
-   						var name2=$(this).attr("id");
-   						document.getElementById(name2).style.backgroundColor = "red";
-
-   						if (document.getElementById(name2).style.backgroundColor = "white") {
-	            		if ( name2!=name1) {
-			            	document.getElementById(name1).style.backgroundColor = "white"
-			            	document.getElementById(name2).style.backgroundColor = "red";
-			            	localStorage.removeItem(name1);
-            				localStorage.setItem("row",name2);
-	            			name1=name2;
-	           			}
-		            	else{
-		            		localStorage.setItem("row",name1);
-		            	 	document.getElementById(name2).style.backgroundColor = "red";
-		            	}
-		            }
-		            	localStorage.setItem("rownew",name);
-   						$('.label2').html(name);
+   						var pa1 = $(this).attr("name");
+   						var pa2 = $(this).attr("id");
+   						changecolor(pa1,pa2);
             		});
 
             	break;
@@ -279,28 +213,10 @@ $( document ).ready(function(){
             		for(var j = 1; j <= 8; j++) {
     		 			$('.label4').html($('.label4').html()+ '<div id="row' + j + '" class="row" name="'+model5[j]+'">'+model5[j]+'</div><br>' );
    					}
-   					var name1="row1";
-   					// document.getElementById(name1).style.backgroundColor = "red";
    					$('.row').click(function(){
-   						var name=$(this).attr("name");
-   						var name2=$(this).attr("id");
-   						document.getElementById(name2).style.backgroundColor = "red";
-
-   						if (document.getElementById(name2).style.backgroundColor = "white") {
-	            		if ( name2!=name1) {
-			            	document.getElementById(name1).style.backgroundColor = "white"
-			            	document.getElementById(name2).style.backgroundColor = "red";
-			            	localStorage.removeItem(name1);
-            				localStorage.setItem("row",name2);
-	            			name1=name2;
-	           			}
-		            	else{
-		            		localStorage.setItem("row",name1);
-		            	 	document.getElementById(name2).style.backgroundColor = "red";
-		            	}
-		            }
-   						localStorage.setItem("rownew",name);
-   						$('.label2').html(name);
+   						var pa1 = $(this).attr("name");
+   						var pa2 = $(this).attr("id");
+   						changecolor(pa1,pa2);
             		});
 
             	break;
@@ -311,28 +227,10 @@ $( document ).ready(function(){
             		for(var j = 1; j <= 1; j++) {
     		 			$('.label4').html($('.label4').html()+ '<div id="row' + j + '" class="row" name="'+model6[j]+'">'+model6[j]+'</div><br>' );
    					}
-   					var name1="row1";
-   					// document.getElementById(name1).style.backgroundColor = "red";
    					$('.row').click(function(){
-   						var name=$(this).attr("name");
-   						var name2=$(this).attr("id");
-   						document.getElementById(name2).style.backgroundColor = "red";
-
-   						if (document.getElementById(name2).style.backgroundColor = "white") {
-	            		if ( name2!=name1) {
-			            	document.getElementById(name1).style.backgroundColor = "white";
-			            	document.getElementById(name2).style.backgroundColor = "red";
-			            	localStorage.removeItem(name1);
-            				localStorage.setItem("row",name2);
-	            			name1=name2;
-	           			}
-		            	else{
-		            		localStorage.setItem("row",name1);
-		            	 	document.getElementById(name2).style.backgroundColor = "red";
-		            	}
-		            }
-		            	localStorage.setItem("rownew",name);
-   						$('.label2').html(name);
+   						var pa1 = $(this).attr("name");
+   						var pa2 = $(this).attr("id");
+   						changecolor(pa1,pa2);
             		});
 
             	break;
@@ -340,20 +238,7 @@ $( document ).ready(function(){
 
         });
 
-		
-		if (localStorage.getItem("cell")!= null) {
-	    	var mycell = localStorage.getItem("cell").split("ll")[1];
-	    	$('.label1').html(model[mycell]);
-
-	    	$('.inputcuoi3').html('<div class="checkboxcell1" style="color:black; background: white;">'+model[mycell]+'</div><br>' );
-
-	    	if (localStorage.getItem("rownew")!= null) {
-	    		localStorage.getItem("rownew",name);
-		    	$('.label2').html(localStorage.getItem("rownew"));
-		    	$('.inputcuoi3').html($('.inputcuoi3').html()+'<div class="checkboxcell2" style="color:black; background: white;">'+localStorage.getItem("rownew")+'</div>' );
-	    	}
-		}
-
+		//show data page result
 		if (localStorage.getItem("check1")!=null) {
 			console.log(localStorage.getItem("check1"));
 
@@ -364,13 +249,12 @@ $( document ).ready(function(){
 			$('.inputcuoi3').html($('.inputcuoi3').html()+'<div class="checkbox2" style="color:black; background: white;">'+localStorage.getItem("check2")+'</div>' );
 			}
 
+		//save data in input in index.html
 		$('.input1').on('input', function() {
     		 localStorage.setItem("name1",$(this).val()) ;
 		});
 
-			var lat1;
-			var rel1;
-			var u=0;
+		//show operation _ in page index2.html
 		$('.lat1').click(function() {
 						lat1=$(this).attr("id");
 						rel3=$('#'+lat1).html();
@@ -385,6 +269,7 @@ $( document ).ready(function(){
 						u=1;
 						});		
 
+		//code keyboard
 		$('.input3').click(function() {
 
 			var res=$(this).attr("id");
@@ -489,15 +374,15 @@ $( document ).ready(function(){
 					}
 
 					if (o==2 && (rel1.split("_")[1])==8) {
-					rel1=rel1.substring(0,5)+o+"_"+"1";
-					rel3=$("#"+rel1).html();
-					if ($("#"+rel1).html("")){
-						$("#"+rel1).html("_");
-						}
-					else if (rel3.charAt(0)!="_") {
-						$("#"+rel1).html(rel3+"_");
-						$('#table3_total').html(rel3+"_");
-						}
+						rel1=rel1.substring(0,5)+o+"_"+"1";
+						rel3=$("#"+rel1).html();
+						if ($("#"+rel1).html("")){
+							$("#"+rel1).html("_");
+							}
+						else if (rel3.charAt(0)!="_") {
+							$("#"+rel1).html(rel3+"_");
+							$('#table3_total').html(rel3+"_");
+							}
 					}
 
 					else if (o==3 && (rel1.split("_")[1])==8) {
@@ -571,9 +456,113 @@ $( document ).ready(function(){
 			break;
 		}
 	});	
-	var rel3;
-	var rel4;
-	var run;
+
+
+		
+		// load data in index1.html
+		function loadlabel(){
+			var modelsuper=new Array;
+			if (localStorage.getItem("cell")!=null) {
+				switch(localStorage.getItem("cell").split("ll")[1]){
+					case "1":
+					modelsuper=model1;
+					break;
+					case "2":
+					modelsuper=model2;
+					break;
+					case "3":
+					modelsuper=model3;
+					break;
+					case "4":
+					modelsuper=model4;
+					break;
+					case "5":
+					modelsuper=model5;
+					break;
+					default:
+					modelsuper=model6;
+					break;
+				}
+			}
+   			for(var i = 1; i <= 6; i++) {
+    		 $('.label3').html($('.label3').html()+ '<div id="cell' + i + '" class="cell">'+model[i]+'</div><br>' );
+
+    		 if(k==0){
+    		 		if (localStorage.getItem("cell")!=null) {
+    		 			var m=localStorage.getItem("cell").split("ll")[1];
+    		 			var g="model"+localStorage.getItem("cell").split("ll")[1];
+
+    		 			for(var t = 1; t <= g.length; t++) {
+	    		 	 	$('.label4').html($('.label4').html()+ '<div id="row' + t + '" class="row" name="'+modelsuper[t]+'">'+modelsuper[t]+'</div><br>');
+	    		 		}
+
+    		 		}
+    		 		else{
+    		 			for(var t = 1; t <= 10; t++) {
+	    		 	 	$('.label4').html($('.label4').html()+ '<div id="row' + t + '" class="row" name="'+model1[t]+'">'+model1[t]+'</div><br>');
+	    		 		}
+    		 		}
+
+				}   
+				if (i==6) {
+					color1();
+					color2();
+
+				}		
+
+    		 }		
+   		}
+
+   		// show data in label1 and page result
+   		if (localStorage.getItem("cell")!= null) {
+	    	var mycell = localStorage.getItem("cell").split("ll")[1];
+	    	$('.label1').html(model[mycell]);
+	    	$('.inputcuoi3').html('<div class="checkboxcell1" style="color:black; background: white;">'+model[mycell]+'</div><br>' );
+
+	    // show data in label2 and page result
+	    	if (localStorage.getItem("rownew")!= null) {
+	    		localStorage.getItem("rownew",name);
+		    	$('.label2').html(localStorage.getItem("rownew"));
+		    	$('.inputcuoi3').html($('.inputcuoi3').html()+'<div class="checkboxcell2" style="color:black; background: white;">'+localStorage.getItem("rownew")+'</div>' );
+	    	}
+		}
+
+		//change color  in table 1 when reload
+		function color1(){
+			if (localStorage.getItem("cell")!=null) {
+	    	document.getElementById(localStorage.getItem("cell")).style.backgroundColor = "red";
+			}
+		}
+		//change color  in table 2 when reload
+		function color2(){
+			if (localStorage.getItem("row")!=null) {
+				console.log(localStorage.getItem("row"));
+	    	document.getElementById(localStorage.getItem("row")).style.backgroundColor = "red";
+			}
+		}
+
+		//change color of table in index1.html
+   		function changecolor(name,name2){
+				document.getElementById(name2).style.backgroundColor = "red";
+
+				if (document.getElementById(name2).style.backgroundColor = "white") {
+	        		if ( name2!=name1) {
+		            	document.getElementById(name1).style.backgroundColor = "white"
+		            	document.getElementById(name2).style.backgroundColor = "red";
+		            	localStorage.removeItem(name1);
+	    				localStorage.setItem("row",name2);
+	        			name1=name2;
+	       			}
+	            	else{
+	            		localStorage.setItem("row",name1);
+	            	 	document.getElementById(name1).style.backgroundColor = "red";
+	            	}
+            	}
+            	localStorage.setItem("rownew",name);
+					$('.label2').html(name);
+
+   		}
+	// show keyboard when click table index.html
 	$('.input2').click(function() {
 		$("#keyboard1").show();
 	   $("#table3").show();
@@ -592,6 +581,7 @@ $( document ).ready(function(){
 
 		});
 
+//exit keyboard when click x (close)
 $('#exit').click(function() {
 
 		rel3=$("#"+rel1).html();
@@ -609,17 +599,18 @@ $('#exit').click(function() {
 	   		$("#table3").hide();
 		}
 		
-	   if (rel3=="_") {
+	   	if (rel3=="_") {
 		    $("#"+rel1).html("");
-	   }
+	   	}
 
-	   else if (rel3.charAt(0)!="_") {
+	   	else if (rel3.charAt(0)!="_") {
 		   	$("#"+rel1).html(rel3.substring(0,rel3.indexOf("_")));
-	   }
+	   	}
 
-	   $("#keyboard1").hide();
-	   $("#table3").hide();
+	   	$("#keyboard1").hide();
+	   	$("#table3").hide();
 });
+
 });
 
 
